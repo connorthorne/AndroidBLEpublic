343aa246bc237898f5d3dc2b7410c1cfd2c11385
connorthorne <connort1002@gmail.com>
2023-02-23 18:20:53 -0500

    working map on maps screen

--- app/src/main/java/com/example/bletutorial/presentation/MapScreen.kt
+++ app/src/main/java/com/example/bletutorial/presentation/MapScreen.kt
@@ -1,11 +1,27 @@
 package com.example.bletutorial.presentation
 
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.rememberCameraPositionState
 
 @Composable
 fun MapScreen(
     navController: NavController
 ) {
-
+    //https://developers.google.com/maps/documentation/android-sdk/maps-compose
+    //https://googlemaps.github.io/android-maps-compose/maps-compose/com.google.maps.android.compose/-google-map.html
+    val singapore = LatLng(1.35, 103.87)
+    val cameraPositionState = rememberCameraPositionState {
+        position = CameraPosition.fromLatLngZoom(singapore, 10f)
+    }
+    GoogleMap(
+        modifier = Modifier.fillMaxSize(),
+        cameraPositionState = cameraPositionState
+    ) {
+    }
 }
